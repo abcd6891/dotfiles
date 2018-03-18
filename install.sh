493fb6f19cb92ead8fc998d83e8f2e6c663b816c
jiayeli <abcd6891@gmail.com>
2018-03-19 00:53:11 +0800

    edit install.sh, install vundle, install font

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-files=$(ls ~/dotfiles)
-echo $files
 
-for file in $files
+for file in vimrc tmux.conf
 do
-	if [ "$file" != "install.sh" ]
-	then
 		ln -s ~/dotfiles/"$file" ~/."$file"
-	fi
 done
+
+echo install vin vundle
+cd ~
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
