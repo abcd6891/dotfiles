4e175bc18256711581ff6a42421945226a15fa08
jiayeli <abcd6891@gmail.com>
2018-03-27 22:55:53 +0800

    add git config in auto install vim env script

--- install.sh
+++ install.sh
@@ -12,3 +12,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/powerline/fonts.git
 cd fonts
 ./install.sh
+
+# git config
+git config --global core.editor vim
+##uncomment if the git is colorless https://unix.stackexchange.com/questions/44266/how-to-colorize-output-of-git
+#git config --global color.ui auto
