374e196bf8c759600e330a52df421c5936fb59b5
jiayeli <jiayeli@synology.com>
2018-08-22 19:22:25 +0800

    New tool install script

---
+++ install_c_dev_tool.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# vim related tools
+sudo apt install vim ctags cscope
+cd ~
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# git related tools
+sudo apt innstall git tig
+
+# git docments
+sudo apt install manpage-dev
---
+++ install_other_tool.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install tools for remote
+sudo apt install nfs-kernel-server tmux openssh-server
+
+# install tools for managements
+sudo apt install util-linux
+
+# install tools for test
+sudo apt install fio
+
+# install tools for other libraries
+sudo apt install curl parallel
